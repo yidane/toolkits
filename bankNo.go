dbad162af10a60c66cf8af6d2d1fa9707807f9ec
yidane <yidane@163.com>
2018-03-12 11:24:57 +0800

    add common func IsBankNo,CreateLuhn

--- bankNo.go
+++ bankNo.go
@@ -3,10 +3,17 @@ package toolkits
 import (
 	"errors"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
-//IsBankNo 是否银行卡账号
+//IsBankNo 是否银行卡账号,主要判断长度是否在16-19之间，验证luhn算法
+//Luhn
+//	检验数字算法（Luhn Check Digit Algorithm），也叫做模数10公式，是一种简单的算法，用于验证银行卡、信用卡号码的有效性的算法。
+//	对所有大型信用卡公司发行的信用卡都起作用，这些公司包括美国Express、护照、万事达卡、Discover和用餐者俱乐部等。
+//	这种算法最初是在20世纪60年代由一组数学家制定，现在Luhn检验数字算法属于大众，任何人都可以使用它。
+//算法：将每个奇数加倍和使它变为单个的数字，如果必要的话通过减去9和在每个偶数上加上这些值。如果此卡要有效，那么，结果必须是10的倍数。
+//http://blog.csdn.net/wangzhjj/article/details/52597614
 func IsBankNo(no string) (bool, error) {
 	no = strings.TrimSpace(no)
 	l := len(no)
@@ -19,9 +26,58 @@ func IsBankNo(no string) (bool, error) {
 		return false, errors.New(`银行卡号必须全为数字`)
 	}
 
-	if !strings.EqualFold(no[0:2], `62`) {
+	//luhn校验
+	total := 0
+	for i := 1; i < len(no); i++ {
+		num, _ := strconv.Atoi(no[i-1 : i])
+		if i%2 == 0 {
+			v := num * 2
+			if v < 9 {
+				total += v
+			} else {
+				total += v - 9
+			}
+		} else {
+			total += num
+		}
+	}
+
+	lastNum, _ := strconv.Atoi(no[len(no)-1:])
+	total += lastNum
+
+	if total%10 != 0 {
 		return false, errors.New(`Luhn校验失败`)
 	}
 
 	return true, nil
+}
+
+//CreateLuhn 使用luhn算法生产luhn验证码
+func CreateLuhn(no string) int {
+	reg, _ := regexp.Compile(`^\d*$`)
+	if !reg.MatchString(no) {
+		return -1
+	}
+
+	total := 0
+	for i := 1; i <= len(no); i++ {
+		num, _ := strconv.Atoi(no[i-1 : i])
+		if i%2 == 0 {
+			v := num * 2
+			if v < 9 {
+				total += v
+			} else {
+				total += v - 9
+			}
+		} else {
+			total += num
+		}
+	}
+
+	k := total % 10
+
+	if k == 0 {
+		return 0
+	}
+	return 10 - k
 }
--- bankNo_test.go
+++ bankNo_test.go
@@ -1,6 +1,8 @@
 package toolkits
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestIsBankNo(t *testing.T) {
 	type args struct {
@@ -11,11 +13,17 @@ func TestIsBankNo(t *testing.T) {
 		want    bool
 		wantErr bool
 	}{
-		{args: "12", want: false, wantErr: true},
+		//{args: "12", want: false, wantErr: true},
+		{args: "6259650871772090", want: true, wantErr: false},
 		{args: "12345678901234567a", want: false, wantErr: true},
-		{args: "12345678901234567890", want: false, wantErr: true},
-		{args: "12345678901234567", want: false, wantErr: true},
-		{args: "62345678901234567", want: true, wantErr: false},
+		{args: "6228480402564890018", want: true, wantErr: false},
+		{args: "6228482298797273578", want: true, wantErr: false},
+		{args: "6212262201023557228", want: true, wantErr: false},
+		{args: "6228481698729890079", want: true, wantErr: false},
+		{args: "6227003325370110828", want: true, wantErr: false},
+		{args: "6217002710000684874", want: true, wantErr: false},
+		{args: "6216616105001489359", want: true, wantErr: false},
+		{args: "6216616105001489351", want: false, wantErr: true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.args, func(t *testing.T) {
@@ -26,6 +34,29 @@ func TestIsBankNo(t *testing.T) {
 			}
 			if got != tt.want {
 				t.Errorf("IsBankNo() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCreateLuhn(t *testing.T) {
+	tests := []struct {
+		arg  string
+		want int
+	}{
+		{arg: "622848040256489001", want: 8},
+		{arg: "622848229879727357", want: 8},
+		{arg: "621226220102355722", want: 8},
+		{arg: "622848169872989007", want: 9},
+		{arg: "622700332537011082", want: 8},
+		{arg: "621700271000068487", want: 4},
+		{arg: "621661610500148935", want: 9},
+		{arg: "62166161050014893a", want: -1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.arg, func(t *testing.T) {
+			if got := CreateLuhn(tt.arg); got != tt.want {
+				t.Errorf("CreateLuhn() = %v, want %v", got, tt.want)
 			}
 		})
 	}
